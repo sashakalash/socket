f66e4ac09fe2ee48fbf68328e9478ebbb4772d99
Aleksandr Koloshtivin <aykolosh@mts.ru>
2021-11-26 19:32:05 +0300

    Chat added

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,22 +1,63 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) {
-        String host = "localhost";
-        int port = 8080;
-        try (Socket clientSocket = new Socket(host, port);
-             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
-        ) {
-            out.println("GET / HTTP/1.1\n" + "Host: netology.ru\n\n\n");
-            String resp = in.readLine();
-            System.out.println(resp);
+    private Socket clientSocket;
+    private DataOutputStream out;
+    private DataInputStream in;
+    private Scanner scanner;
+
+    public void startConnection(String host, int port) {
+        try {
+            clientSocket = new Socket(host, port);
+            out = new DataOutputStream(clientSocket.getOutputStream());
+            in = new DataInputStream(clientSocket.getInputStream());
+            scanner = new Scanner(System.in);
+
+            System.out.println("Client connected to socket");
+
+            while (!clientSocket.isOutputShutdown()) {
+                String input = in.readUTF();
+                System.out.println("Server: " + input);
+                System.out.println("You: ");
+                String inputString = sendMessage();
+
+                if (inputString != null) {
+                    out.writeUTF(inputString);
+                    out.flush();
+
+                    if (inputString.equalsIgnoreCase("quit")) {
+                        System.out.println("You've just left the chat");
+                        break;
+                    }
+                }
+            }
+            stopConnection();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+    }
+
+    public String sendMessage() throws IOException {
+        return scanner.nextLine();
+    }
+
+    public void stopConnection() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+        System.out.println("Closing connections & channels on clentSide - DONE.");
+    }
+
+    public static void main(String[] args) {
+        String host = "netology.homework";
+        int port = 8080;
+        Client clientServer = new Client();
+        clientServer.startConnection(host, port);
     }
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,25 +1,60 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private DataOutputStream out;
+    private DataInputStream in;
+    String name;
 
-    public static void main(String[] args) throws IOException {
-        final int PORT = 8080;
-        ServerSocket serverSocket;
-        serverSocket  = new ServerSocket(PORT);
-        Socket clientSocket = serverSocket.accept();
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.startConnection(8080);
+    }
 
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    public void startConnection(int port) {
+        try {
+            serverSocket= new ServerSocket(port);
+            clientSocket = serverSocket.accept();
+            System.out.println("Connection accepted");
 
-        System.out.println("New connection accepted");
+            out = new DataOutputStream(clientSocket.getOutputStream());
+            in = new DataInputStream(clientSocket.getInputStream());
 
-        final String name = in.readLine();
+            out.writeUTF("What's your name?");
+            name = in.readUTF();
 
-        out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+            while (!clientSocket.isClosed()) {
+                out.writeUTF("Are you child? (yes/no)");
+                String entry = in.readUTF();
+                if(entry.equalsIgnoreCase("quit")){
+                    System.out.println("Client has left the chat");
+                    out.writeUTF("Server reply - " + entry + " - OK");
+                    out.flush();
+                    break;
+                } else if (entry.equalsIgnoreCase("yes")) {
+                    out.writeUTF("Welcome to the kids area, " + name + "! Let's play!");
+                } else if (entry.equalsIgnoreCase("no")) {
+                    out.writeUTF("Welcome to the adult zone, " + name + "! Have a good rest, or a good working day!");
+                } else {
+                    out.writeUTF("I didn't get you, " + name + ". Try again!");
+                }
+                out.flush();
+            }
+            stopConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void stopConnection() throws IOException {
+        System.out.println("Closing connections & channels");
+        in.close();
+        out.close();
+        clientSocket.close();
+        serverSocket.close();
     }
 }
